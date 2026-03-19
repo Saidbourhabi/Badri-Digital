013516ce01768997818c7de0d260cd95b6967039
Said-bourhabi <abousaid11araqi@gmail.com>
2026-03-19 21:40:38 +0000

    Initial commit: Badri Digital portfolio setup with React and Tailwind css and reactrouter and gsap + swiper

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      {/* Page Routes */}
+    </Routes>
+    </>
+  )
+}
+
+export default App
