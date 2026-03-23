bba2cbc7d6fa77e621d894deaef8e107c17c3df4
Said-bourhabi <abousaid11araqi@gmail.com>
2026-03-23 04:45:32 +0100

    feat: add React pages - Home, About, Portfolio , Blog , Article, Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { Route, Routes } from 'react-router-dom'
 
 // pages imports
 import Home from './pages/home/home'
+import Portfolio from './pages/portfolio/portfolio'
+import About from './pages/about/about'
+import Blog from './pages/blog/blog'
+import Article from './pages/blog/article'
+import Contact from './pages/contact/contact'
 // 404 page
 import NotFound from './pages/NotFound'
 // layouts impots
@@ -16,6 +21,12 @@ function App() {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/blog/:slug" element={<Article />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
         {/*  404 page route */}
       <Route path="*" element={<NotFound />} />
     </Routes>
